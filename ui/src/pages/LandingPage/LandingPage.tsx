ae1666b01eda69344b1b2be6b6312a6a14ddbf78
diego <dfmontesinos@gmail.com>
2024-08-18 19:55:30 +0200

    adding more functionality to the landing page

--- ui/src/pages/LandingPage/LandingPage.tsx
+++ ui/src/pages/LandingPage/LandingPage.tsx
@@ -1,19 +1,34 @@
 import Navbar from "../NavBar/NavBar";
 import "./LandingPage.css";
-
+import pokeball from "../../images/images.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <Navbar />
+      <div className="text-container">welcome to you Pokedex App</div>
       <div id="image-container">
+        <p>
+          This is you Pokedex App, the place to search, found, and catch all
+          your pokemons, like Ash, if you wan't to be the very best, you have to
+          catch en all
+        </p>
         <img
-          src="../../images/images.jpeg"
+          src={pokeball}
           alt="Pokemones y Masterball"
           className="pokemon-image"
         />
       </div>
-      <div id="container">Welcome to the Pokedex API DFM</div>
+      <div className="button-container">
+        <p>To start, let's register</p>
+        <button>
+          <a href="./">
+            <FontAwesomeIcon icon={faSignOutAlt} /> Click to login
+          </a>
+        </button>
+      </div>
     </div>
   );
 };
